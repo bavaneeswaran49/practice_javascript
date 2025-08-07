f4a6e5392a1d79ed60b26da4db5a5ad6fa1f72c0
Bavaneeswar K <bavaneeswaran49@gmail.com>
2025-08-07 18:53:26 +0530

    Add files via upload
    array types in push,pop,unshift,shift,Indecof and LastIndexof

---
+++ j/arrayp1.js
@@ -0,0 +1,34 @@
+//push
+let a=["suriya",1,"praveen",2,"bavan",3,"vikram",4];
+console.log(a);
+a.push("karapu",5);
+console.log(a);
+
+
+
+//pop
+let b=["bavan","balaji","bhavan","bablu"];
+console.log(b);
+b.pop();
+console.log(b);
+
+//shift
+let num=[1,2,3,4,5];
+num.shift();
+console.log(num);
+
+
+//unshift 
+let fruits=["apple","pineapple","banana"]
+fruits.unshift("grapls","Mango")
+console.log(fruits);
+
+//indexof
+let school=["vikas","Avp","Infant"]
+console.log(school.indexOf("vikas"));
+
+
+//lastIndexof
+let Name=["sathi","anu","anitha","anu"];
+i=Name.lastIndexOf("anu");
+console.log(i);
